cf4d3e744958912a5b9dac861181167263c4faf1
Patate_Douce <72559937+AdrienVirieux@users.noreply.github.com>
2022-03-16 23:04:14 +0100

    remettre le tous dans un seul fichier js

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,3 @@
-import { graph, initGraph, updateGraph } from './graph.js'
-import { connectToServeur } from './connect.js';
-import { initLocalStorage } from './storage.js';
-
-
-
 // Mise en place du Service Worker
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
@@ -110,8 +104,12 @@ Array.from(document.querySelectorAll('section p img')).forEach(function(onglet) 
 });
 
 
+
+
 // --------------------------------------------------------------------------------------------- //
 // --------------------------------------------------------------------------------------------- //
+
+
 
 // Initialise l'affichage des valeurs lors que l'on refresh la page
 function initDisplayValue() {
@@ -142,6 +140,64 @@ function setDisplayMinMax() {
 }
 
 
+
+// --------------------------------------------------------------------------------------------- //
+
+// Notifications
+function alertNotification(texte, temperature) {
+    let notifTitle = "Alerte température !";
+    let notifBody = 'Température : ' + temperature + '.\n' + texte;
+    let notifImg = '/assets/images/android-chrome-192x192.png';
+    let options = {
+        body: notifBody,
+        icon: notifImg
+    }
+    Notification(notifTitle, options);
+}
+
+const buttonNotifPush = document.getElementById("notifications");
+buttonNotifPush.addEventListener('click', function(e) {
+    Notification.requestPermission().then(function(result) {
+        if (result === 'granted') {
+            alertNotification('test', 12.9)
+        }
+    });
+});
+
+// --------------------------------------------------------------------------------------------- //
+
+// bouton d'installation
+let deferredPrompt;
+const addBtn = document.getElementById('add-button');
+addBtn.style.display = 'block';
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    // Prevent Chrome 67 and earlier from automatically showing the prompt  
+    e.preventDefault();
+    // Stash the event so it can be triggered later.  
+    deferredPrompt = e;
+    // Update UI to notify the user they can add to home screen  
+    addBtn.style.display = 'block';
+
+    addBtn.addEventListener('click', (e) => {
+        // hide our user interface that shows our A2HS button  
+        addBtn.style.display = 'none';
+        // Show the prompt  
+        deferredPrompt.prompt();
+        // Wait for the user to respond to the prompt  
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('User accepted the A2HS prompt');
+            } else {
+                console.log('User dismissed the A2HS prompt');
+            }
+            deferredPrompt = null;
+        });
+    });
+});
+
+// --------------------------------------------------------------------------------------------- //
+
 // Fonctions implémentée
 function sideMenu() {
     let sideNav = document.getElementById('side-nav');
@@ -157,100 +213,79 @@ function sideMenu() {
 // Une thes belle alerte alerte
 // window.alert("Va te faire enculé sale fils de pute");
 
-// ------------------------------------------------------------------ //
-// ------------------------------------------------------------------ //
+// --------------------------------------------------------------------------------------------- //
+// --------------------------------------------------------------------------------------------- //
 
 
 
 
-
-// ------------------------------------------------------------------ //
-// ------------------------------------------------------------------ //
-
-
-// Ajoute les valeurs du serveur recu dans le localStorage
-function localCacheTemp(temperatureIn, temperatureOut) {
-    let arraysJSON = JSON.parse(localStorage.getItem('TEMPERATURE'));
-    let tempIn = arraysJSON['IN_TEMP'];
-    let tempOut = arraysJSON['OUT_TEMP'];
-
-    // Instanciation des variables de Date
-    var today = new Date();
-    // var yesterday = new Date().addHours(-1);
-
-    // // Enlèvement des valeurs que l'on veut pas
-    // for (var i = 0; i < Object.keys(tempIn['TEMP']).length; ++i) {
-    //     if (tempIn['TIME'][i] < yesterday) {
-    //         tempIn['TEMP'].pop(i);
-    //         tempIn['TIME'].pop(i);
-    //     }
-    //     if (tempOut['TIME'][i] < yesterday) {
-    //         tempOut['TEMP'].pop(i)
-    //         tempOut['TIME'].pop(i)
-    //     }
-    // }
-
-    // Ajout de la nouvelle température dans l'array respectif
-    tempIn['TEMP'].push(temperatureIn);
-    tempOut['TEMP'].push(temperatureOut);
-
-    // Ajout de la date	dans l'array respectif
-    tempIn['TIME'].push(today);
-    tempOut['TIME'].push(today);
-
-    // Modification de la valeur minimum et maximum
-    if (tempIn['MIN'] == null || tempIn['MIN'] > temperatureIn) tempIn['MIN'] = temperatureIn;
-    if (tempIn['MAX'] == null || tempIn['MAX'] < temperatureIn) tempIn['MAX'] = temperatureIn;
-
-    if (tempOut['MIN'] == null || tempOut['MIN'] > temperatureOut) tempOut['MIN'] = temperatureOut;
-    if (tempOut['MAX'] == null || tempOut['MAX'] < temperatureOut) tempOut['MAX'] = temperatureOut;
-
-    //
-    console.log(arraysJSON);
-
-    // Ajout valeurs dans le localStorage
-    localStorage.setItem('TEMPERATURE', JSON.stringify(arraysJSON));
-}
+connectToServeur();
+initGraph();
+var interval = setInterval(function() {
+    connectToServeur();
+}, 300000);
 
 
-// Permet de supprimer les valeurs mise en 'cache' (utiliser par notre graphe)
-function removeTempStorage() {
-    let structJSON = {
-        'IN_TEMP': {
-            'MIN': null,
-            'MAX': null,
-            'TEMP': [],
-            'TIME': []
-        },
-        'OUT_TEMP': {
-            'MIN': null,
-            'MAX': null,
-            'TEMP': [],
-            'TIME': []
+
+
+// --------------------------------------------------------------------------------------------- //
+// --------------------------------------------------------------------------------------------- //
+
+
+
+// WEBSOCKET
+function connectToServeur() {
+    // Initialisation de la Websocket
+    const socket = new WebSocket('wss://ws.hothothot.dog:9502');
+
+
+    // Ajout d'un listener pour les possibles erreurs de la Websocket
+    socket.addEventListener('error', function(event) {
+        console.log("Problème de connection rencontré avec Websocket. Tentative de reconnection avec Fetch...");
+
+        // On utilise alors la méthode Fetch
+        fetch("https://hothothot.dog/api/capteurs?format=json", { method: "POST" })
+            .then(response => {
+                if (response.ok) {
+                    return response.json(); // Convertion du message recu en JSON
+                }
+            })
+            .then(function(data) {
+                console.log('Objet recu du serveur (Fetch) :', data);
+                try { getTemperature(data) } catch (e) { console.log(e) }
+            })
+            .catch((error) => console.log('Problème de connection rencontré avec Fetch'))
+    });
+
+
+    // Connection au server avec Websocket
+    socket.onopen = function(event) {
+        console.log("Connexion Websocket établie");
+
+        // Envoi d'un message au serveur (obligatoire)
+        socket.send("couscous");
+        socket.onmessage = function(msg) {
+            // Convertion du message recu en JSON
+            var resultJson = JSON.parse(msg.data);
+
+            console.log('Objet recu du serveur (Websocket) :', resultJson);
+            try { getTemperature(resultJson) } catch (e) { console.log(e) }
         }
-    };
-    localStorage.setItem('TEMPERATURE', JSON.stringify(structJSON));
-    updateGraph();
+    }
 }
 
 
-// -----------------------------------------------
-
-
-
-// -----------------------------------------------
-// 
 var span_in_temperature = document.getElementById('span_in_temperature');
 var span_out_temperature = document.getElementById('span_out_temperature');
 let content = document.querySelector('template').content;
 
 //
-export function getTemperature(dataJSON) {
+function getTemperature(dataJSON) {
     let temperatureIn = dataJSON["capteurs"]["0"]["Valeur"];
     let temperatureOut = dataJSON["capteurs"]["1"]["Valeur"];
 
     if (parseFloat(temperatureIn) == 'NaN' && parseFloat(temperatureOut) == 'NaN') return;
-    localCacheTemp(temperatureIn, temperatureOut);
+    addTemperatureInLocalCache(temperatureIn, temperatureOut);
 
     // Couleur utilisé suivant la température extérieure
     let color = 'white';
@@ -316,71 +351,191 @@ export function getTemperature(dataJSON) {
 }
 
 
-// ------------------------------------------------------------------ //
-// ------------------------------------------------------------------ //
 
 
-connectToServeur();
-initGraph();
-var interval = setInterval(function() {
-    connectToServeur();
-}, 300000);
-
-
-// ------------------------------------------------------------------ //
-// ------------------------------------------------------------------ //
+// --------------------------------------------------------------------------------------------- //
+// --------------------------------------------------------------------------------------------- //
 
 
 
-
-// Notifications
-function alertNotification(text, temperature) {
-    var notifTitle = "Alerte température !";
-    var notifBody = 'Température : ' + temperature + '.\n' + text;
-    var notifImg = '/assets/images/android-chrome-192x192.png';
-    var options = {
-        body: notifBody,
-        icon: notifImg
-    }
-    var notif = new Notification(notifTitle, options);
+// Définie une structure JSON vide, que l'on affecte au LocalStorage 'TEMPERATURE'
+function initLocalStorage() {
+    let structStorageJSON = {
+        'DATE': null,
+        'IN_TEMP': {
+            'MIN': null,
+            'MAX': null,
+            'TEMP': [],
+            'TIME': []
+        },
+        'OUT_TEMP': {
+            'MIN': null,
+            'MAX': null,
+            'TEMP': [],
+            'TIME': []
+        }
+    };
+    localStorage.setItem('TEMPERATURE', JSON.stringify(structStorageJSON));
 }
 
-var button = document.getElementById("notifications");
-button.addEventListener('click', function(e) {
-    Notification.requestPermission().then(function(result) {
-        if (result === 'granted') {
-            alertNotification('test', 12.9)
+// Ajoute les valeurs du serveur recu dans le localStorage
+function addTemperatureInLocalCache(temperatureIn, temperatureOut) {
+    let arraysJSON = JSON.parse(localStorage.getItem('TEMPERATURE'));
+    let tempIn = arraysJSON['IN_TEMP'];
+    let tempOut = arraysJSON['OUT_TEMP'];
+
+    // Instanciation des variables de Date
+    var today = new Date();
+    // var yesterday = new Date().addHours(-1);
+
+    // // Enlèvement des valeurs que l'on veut pas
+    // for (var i = 0; i < Object.keys(tempIn['TEMP']).length; ++i) {
+    //     if (tempIn['TIME'][i] < yesterday) {
+    //         tempIn['TEMP'].pop(i);
+    //         tempIn['TIME'].pop(i);
+    //     }
+    //     if (tempOut['TIME'][i] < yesterday) {
+    //         tempOut['TEMP'].pop(i)
+    //         tempOut['TIME'].pop(i)
+    //     }
+    // }
+
+    // Ajout de la nouvelle température dans l'array respectif
+    tempIn['TEMP'].push(temperatureIn);
+    tempOut['TEMP'].push(temperatureOut);
+
+    // Ajout de la date	dans l'array respectif
+    tempIn['TIME'].push(today);
+    tempOut['TIME'].push(today);
+
+    // Modification de la valeur minimum et maximum
+    if (tempIn['MIN'] == null || tempIn['MIN'] > temperatureIn) tempIn['MIN'] = temperatureIn;
+    if (tempIn['MAX'] == null || tempIn['MAX'] < temperatureIn) tempIn['MAX'] = temperatureIn;
+
+    if (tempOut['MIN'] == null || tempOut['MIN'] > temperatureOut) tempOut['MIN'] = temperatureOut;
+    if (tempOut['MAX'] == null || tempOut['MAX'] < temperatureOut) tempOut['MAX'] = temperatureOut;
+
+    //
+    console.log(arraysJSON);
+
+    // Ajout valeurs dans le localStorage
+    localStorage.setItem('TEMPERATURE', JSON.stringify(arraysJSON));
+}
+
+
+// Permet de supprimer les valeurs mise en 'cache' (utiliser par notre graphe)
+function removeTempStorage() {
+    let structJSON = {
+        'IN_TEMP': {
+            'MIN': null,
+            'MAX': null,
+            'TEMP': [],
+            'TIME': []
+        },
+        'OUT_TEMP': {
+            'MIN': null,
+            'MAX': null,
+            'TEMP': [],
+            'TIME': []
+        }
+    };
+    localStorage.setItem('TEMPERATURE', JSON.stringify(structJSON));
+    updateGraph();
+}
+
+
+
+
+// --------------------------------------------------------------------------------------------- //
+// --------------------------------------------------------------------------------------------- //
+// GRAPH-HISTORIQUE
+
+
+let graph;
+
+// Source : https://stackoverflow.com/questions/1050720/adding-hours-to-javascript-date-object
+Date.prototype.addHours = function (h) {
+    this.setTime(this.getTime() + (h * 60 * 60 * 1000));
+    return this;
+}
+
+//
+function initGraph() {
+    let date = new Date().addHours(-24);
+
+    // Initialisation d'une base d'un graphe
+    graph = new Chart(document.getElementById("myChart"), {
+        type: 'scatter',
+        data: {
+            datasets: [{
+                label: "T° intérieure",
+                borderColor: 'blue',
+                borderWidth: 2,
+                pointRadius: 0,
+                fill: false,
+                tension: 0.05,
+                showLine: true
+            },
+            {
+                label: "T° extérieure",
+                borderColor: 'red',
+                borderWidth: 2,
+                pointRadius: 0,
+                fill: false,
+                tension: 0.05,
+                showLine: true
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                y: {
+                    min: 0,
+                },
+                x: {
+                    type: 'time',
+                    time: {
+                        unit: 'hour',
+                        displayFormats: {
+                            'hour': 'dd/MM/yyyy HH:mm'
+                        }
+                    },
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Time'
+                    }
+                }
+            }
         }
     });
-});
 
-// -----------------------------------------------
-// bouton d'installation
-let deferredPrompt;
-const addBtn = document.getElementById('add-button');
-addBtn.style.display = 'block';
+    // Ajout des anciennes valeures mise en cache
+    let arraysCacheJSON = JSON.parse(localStorage.getItem('TEMPERATURE'));
+    let tempIn = arraysCacheJSON['IN_TEMP'];
+    let tempOut = arraysCacheJSON['OUT_TEMP'];
 
-window.addEventListener('beforeinstallprompt', (e) => {
-    // Prevent Chrome 67 and earlier from automatically showing the prompt  
-    e.preventDefault();
-    // Stash the event so it can be triggered later.  
-    deferredPrompt = e;
-    // Update UI to notify the user they can add to home screen  
-    addBtn.style.display = 'block';
+    for (var i = 0; i < Object.keys(tempIn['TEMP']).length; ++i) {
+        graph.data.datasets[0].data.push({ 'x': tempIn['TIME'][i], 'y': tempIn['TEMP'][i] });
+        graph.data.datasets[1].data.push({ 'x': tempIn['TIME'][i], 'y': tempOut['TEMP'][i] });
+    }
+    graph.update();
 
-    addBtn.addEventListener('click', (e) => {
-        // hide our user interface that shows our A2HS button  
-        addBtn.style.display = 'none';
-        // Show the prompt  
-        deferredPrompt.prompt();
-        // Wait for the user to respond to the prompt  
-        deferredPrompt.userChoice.then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted') {
-                console.log('User accepted the A2HS prompt');
-            } else {
-                console.log('User dismissed the A2HS prompt');
-            }
-            deferredPrompt = null;
-        });
-    });
-});
+    console.log(graph.data.datasets);
+}
+
+
+//
+function updateGraph() {
+    let arraysCacheJSON = JSON.parse(localStorage.getItem('TEMPERATURE'));
+    let tempIn = arraysCacheJSON['IN_TEMP'];
+    let tempOut = arraysCacheJSON['OUT_TEMP'];
+
+    let i = Object.keys(tempIn['TEMP']).length - 1;
+
+    graph.data.datasets[0].data.push({ 'x': tempIn['TIME'][i], 'y': tempIn['TEMP'][i] });
+    graph.data.datasets[1].data.push({ 'x': tempIn['TIME'][i], 'y': tempOut['TEMP'][i] });
+
+    graph.update();
+}
+
