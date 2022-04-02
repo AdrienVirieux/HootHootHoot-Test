f5d02a4a6a98cb43bccf5a7c4e8b53a4bec7879e
Patate_Douce <72559937+AdrienVirieux@users.noreply.github.com>
2022-04-02 16:17:50 +0200

    push avant le désastre

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,26 @@ if ('serviceWorker' in navigator) {
 
 // --------------------------------------------------------------------------------------------- //
 
+
+// Check si une personne s'est connecté à son compte
+if (sessionStorage.getItem('status') == null) {
+    let loginPage = document.getElementById('login-background');
+    // loginPage.style.height = '100%';
+}
+
+
+
+// Permet de savoir si l'utilisateur est en-ligne ou  hors-ligne
+window.addEventListener('load', function (e) {
+    if (navigator.onLine) {
+        console.log('We\'re online!');
+    } else {
+        console.log('We\'re offline...');
+    }
+}, false);
+
+
+
 // Initialisation du LocalStorage lorsque l'on se rend sur l'app pour la première fois
 if (localStorage.getItem('TEMPERATURE') == null) {
     initLocalStorage();
@@ -41,7 +61,7 @@ var weekday = toTitleCase(today.toLocaleDateString("fr-FR", opt_weekday));
 function toTitleCase(str) {
     return str.replace(
         /\w\S*/g,
-        function(txt) {
+        function (txt) {
             return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
         }
     );
@@ -55,15 +75,15 @@ document.getElementById('titreTemp').innerText = the_date;
 // LISTENERS
 
 // Listener pour le changement d'onglet
-Array.from(document.querySelectorAll('#nav-onglet li, #nav-onglet span, #nav-onglet div')).forEach(function(onglet) {
+Array.from(document.querySelectorAll('#nav-onglet li, #nav-onglet span, #nav-onglet div')).forEach(function (onglet) {
     onglet.addEventListener('click',
-        function(event) {
+        function (event) {
             // Suppression de l'onglet actif
-            Array.from(document.querySelectorAll('.active')).forEach(function(elem_active) {
+            Array.from(document.querySelectorAll('.active')).forEach(function (elem_active) {
                 elem_active.removeAttribute('class');
             });
             // Suppression du boutton d'onglet actif
-            Array.from(document.querySelectorAll('.onglet-actif')).forEach(function(elem_active) {
+            Array.from(document.querySelectorAll('.onglet-actif')).forEach(function (elem_active) {
                 elem_active.removeAttribute('class');
             });
 
@@ -89,10 +109,10 @@ Array.from(document.querySelectorAll('#nav-onglet li, #nav-onglet span, #nav-ong
 
 // Listener pour définir la température interieure ou exterieure à mettre en favori
 // Change les valeurs de Min et de Max
-Array.from(document.querySelectorAll('section p img')).forEach(function(onglet) {
+Array.from(document.querySelectorAll('section p img')).forEach(function (onglet) {
     onglet.addEventListener('click',
-        function(event) {
-            Array.from(document.querySelectorAll('.favori')).forEach(function(elem_active) {
+        function (event) {
+            Array.from(document.querySelectorAll('.favori')).forEach(function (elem_active) {
                 elem_active.removeAttribute('class');
                 elem_active.setAttribute('src', 'img/star-empty.png')
             });
@@ -103,6 +123,20 @@ Array.from(document.querySelectorAll('section p img')).forEach(function(onglet) 
     );
 });
 
+
+// Listener pour accepter la connection à un compte ou non
+document.getElementById('tryLogin').addEventListener('click',
+    function () {
+        
+
+        let loginPage = document.getElementById('login-background');
+        loginPage.style.padding = '0 25% 0 25%';
+        loginPage.style.height = '0';
+        setTimeout(function() {
+            document.getElementById('login-page').style.display = 'none';
+        }, 950);
+    }
+);
 
 
 
@@ -117,6 +151,9 @@ function initDisplayValue() {
     let lastTemperatureIn = temperatureLocalStorageJSON['IN_TEMP']['TEMP'][lastIndex];
     let lastTemperatureOut = temperatureLocalStorageJSON['OUT_TEMP']['TEMP'][lastIndex];
 
+    // Couleur de la température à afficher suivant sa valeur 
+    setColorTemperature(lastTemperatureIn, lastTemperatureOut);
+
     // Insertion des temperatures
     interiorDisplay.innerText = lastTemperatureIn + '°C';
     exteriorDisplay.innerText = lastTemperatureOut + '°C';
@@ -126,7 +163,7 @@ function initDisplayValue() {
 }
 
 
-//
+// Permet d'afficher les valeurs minimum et maximum atteinte suivant notre favori
 function setDisplayMinMax() {
     let temperature = temperatureLocalStorageJSON;
     if (document.getElementsByClassName('favori')[0].parentNode.id == 'p_in_temperature') {
@@ -135,8 +172,8 @@ function setDisplayMinMax() {
         temperature = temperature['OUT_TEMP'];
     }
 
-    spanMin.innerText = temperature['MIN'];
-    spanMax.innerText = temperature['MAX'];
+    spanMin.innerText = temperature['MIN'] + '°C';
+    spanMax.innerText = temperature['MAX'] + '°C';
 }
 
 
@@ -152,12 +189,12 @@ function alertNotification(texte, temperature) {
         body: notifBody,
         icon: notifImg
     }
-    Notification(notifTitle, options);
+    new Notification(notifTitle, options);
 }
 
 const buttonNotifPush = document.getElementById("notifications");
-buttonNotifPush.addEventListener('click', function(e) {
-    Notification.requestPermission().then(function(result) {
+buttonNotifPush.addEventListener('click', function (e) {
+    Notification.requestPermission().then(function (result) {
         if (result === 'granted') {
             alertNotification('test', 12.9)
         }
@@ -224,7 +261,7 @@ let graph;
 
 connectToServeur();
 initGraph();
-var interval = setInterval(function() {
+var interval = setInterval(function () {
     connectToServeur();
 }, 300000);
 
@@ -233,129 +270,7 @@ var interval = setInterval(function() {
 
 // --------------------------------------------------------------------------------------------- //
 // --------------------------------------------------------------------------------------------- //
-
-
-
-// WEBSOCKET
-function connectToServeur() {
-    // Initialisation de la Websocket
-    const socket = new WebSocket('wss://ws.hothothot.dog:9502');
-
-
-    // Ajout d'un listener pour les possibles erreurs de la Websocket
-    socket.addEventListener('error', function(event) {
-        console.log("Problème de connection rencontré avec Websocket. Tentative de reconnection avec Fetch...");
-
-        // On utilise alors la méthode Fetch
-        fetch("https://hothothot.dog/api/capteurs?format=json", { method: "POST" })
-            .then(response => {
-                if (response.ok) {
-                    return response.json(); // Convertion du message recu en JSON
-                }
-            })
-            .then(function(data) {
-                console.log('Objet recu du serveur (Fetch) :', data);
-                try { getTemperature(data) } catch (e) { console.log(e) }
-            })
-            .catch((error) => console.log('Problème de connection rencontré avec Fetch'))
-    });
-
-
-    // Connection au server avec Websocket
-    socket.onopen = function(event) {
-        console.log("Connexion Websocket établie");
-
-        // Envoi d'un message au serveur (obligatoire)
-        socket.send("couscous");
-        socket.onmessage = function(msg) {
-            // Convertion du message recu en JSON
-            var resultJson = JSON.parse(msg.data);
-
-            console.log('Objet recu du serveur (Websocket) :', resultJson);
-            try { getTemperature(resultJson) } catch (e) { console.log(e) }
-        }
-    }
-}
-
-
-//
-function getTemperature(dataJSON) {
-    let temperatureIn = dataJSON["capteurs"]["0"]["Valeur"];
-    let temperatureOut = dataJSON["capteurs"]["1"]["Valeur"];
-
-    if (parseFloat(temperatureIn) == 'NaN' && parseFloat(temperatureOut) == 'NaN') return;
-    addTemperatureInLocalCache(temperatureIn, temperatureOut);
-
-    // Couleur utilisé suivant la température extérieure
-    let color = 'white';
-    if (temperatureOut < 0)
-        color = 'blue';
-    else if (35 < temperatureOut)
-        color = 'red';
-    span_out_temperature.setAttribute("class", color);
-    span_out_temperature.innerText = temperatureOut;
-
-    // Couleur utilisé suivant la température intérieure
-    color = 'white'
-    if (temperatureIn < 0)
-        color = 'blue';
-    else if (0 <= temperatureIn && temperatureIn < 12)
-        color = 'cyan';
-    else if (22 <= temperatureIn && temperatureIn < 50)
-        color = 'orange';
-    else if (50 <= temperatureIn)
-        color = 'red';
-    else
-        color = 'white';
-    span_in_temperature.setAttribute("class", color);
-    span_in_temperature.innerText = temperatureIn;
-
-    setDisplayMinMax();
-
-    updateGraph();
-
-    // Affichage des problèmes
-    let allMsg = []
-
-    if (temperatureOut < 0) {
-        allMsg.push('Banquise en vue !');
-    } else if (35 < temperatureOut) {
-        allMsg.push('Hot ! Hot ! Hot !');
-    }
-
-    if (temperatureIn < 0) {
-        allMsg.push('Canalisations gelées, appelez SOS plombier et mettez un bonnet !');
-    } else if (0 <= temperatureIn && temperatureIn < 12) {
-        allMsg.push('Montez le chauffage ou mettez un gros pull  !');
-    } else if (22 < temperatureIn && temperatureIn <= 50) {
-        allMsg.push('Baissez le chauffage !');
-    } else if (50 <= temperatureIn) {
-        allMsg.push('Appelez les pompiers ou arrêtez votre barbecue !');
-    }
-
-    let alertDiv = document.getElementById('contentAlert');
-    // Remove toutes les alertes précédentes
-    while (alertDiv.lastElementChild) {
-        alertDiv.removeChild(alertDiv.lastElementChild);
-    }
-
-    // Ajout des nouvelles alertes
-    for (i in allMsg.length) {
-        alertDiv.appendChild(document.importNode(content, true));
-        let span = document.querySelectorAll('#contentAlert section:last-of-type span')[0];
-        span.innerText = allMsg[i];
-        // Changer l'affichage de la température
-        alertNotification(allMsg[i], temperatureIn);
-    }
-}
-
-
-
-
-// --------------------------------------------------------------------------------------------- //
-// --------------------------------------------------------------------------------------------- //
-
-
+// LOCALSTORAGE
 
 // Définie une structure JSON vide, que l'on affecte au LocalStorage 'TEMPERATURE'
 function initLocalStorage() {
@@ -376,6 +291,7 @@ function initLocalStorage() {
     };
     localStorage.setItem('TEMPERATURE', JSON.stringify(structStorageJSON));
 }
+
 
 // Ajoute les valeurs du serveur recu dans le localStorage
 function addTemperatureInLocalCache(temperatureIn, temperatureOut) {
@@ -423,26 +339,144 @@ function addTemperatureInLocalCache(temperatureIn, temperatureOut) {
 
 
 // Permet de supprimer les valeurs mise en 'cache' (utiliser par notre graphe)
-function removeTempStorage() {
-    let structJSON = {
-        'IN_TEMP': {
-            'MIN': null,
-            'MAX': null,
-            'TEMP': [],
-            'TIME': []
-        },
-        'OUT_TEMP': {
-            'MIN': null,
-            'MAX': null,
-            'TEMP': [],
-            'TIME': []
-        }
-    };
-    localStorage.setItem('TEMPERATURE', JSON.stringify(structJSON));
-    updateGraph();
+function removeLocalStorage() {
+    try {
+        initLocalStorage();
+        updateGraph();
+        console.log("Réinitialisation des valeurs du localStorage");
+    }
+    catch (exception) {
+        console.log(exception);
+    }
 }
 
 
+
+
+// --------------------------------------------------------------------------------------------- //
+// --------------------------------------------------------------------------------------------- //
+// WEBSOCKET
+
+// On essai de se connecter au serveur ayant les données avec une WebSocket
+// Si on a une erreur, on les récupère avec Fetch
+function connectToServeur() {
+    const socket = new WebSocket('wss://ws.hothothot.dog:9502');
+
+    // Ajout d'un listener pour les possibles erreurs de la Websocket
+    socket.addEventListener('error', function (event) {
+        console.log("Problème de connection rencontré avec Websocket. Tentative de reconnection avec Fetch...");
+
+        // On utilise alors la méthode Fetch
+        fetch("https://hothothot.dog/api/capteurs?format=json", { method: "POST" })
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+            })
+            .then(function (data) {
+                console.log('Objet recu du serveur (Fetch) :', data);
+                try {
+                    getTemperature(data)
+                }
+                catch (e) {
+                    console.log(e)
+                }
+            })
+            .catch((error) => console.log('Problème de connection rencontré avec Fetch'))
+    });
+
+    // Connection au server avec Websocket
+    socket.onopen = function (event) {
+        console.log("Connexion Websocket établie");
+
+        // Envoi d'un message au serveur (obligatoire)
+        socket.send("couscous");
+        socket.onmessage = function (msg) {
+            var resultJson = JSON.parse(msg.data);
+
+            console.log('Objet recu du serveur (Websocket) :', resultJson);
+            try { getTemperature(resultJson) } catch (e) { console.log(e) }
+        }
+    }
+}
+
+
+//
+function getTemperature(dataJSON) {
+    let temperatureIn = dataJSON["capteurs"]["0"]["Valeur"];
+    let temperatureOut = dataJSON["capteurs"]["1"]["Valeur"];
+
+    if (parseFloat(temperatureIn) == 'NaN' && parseFloat(temperatureOut) == 'NaN') return;
+    addTemperatureInLocalCache(temperatureIn, temperatureOut);
+
+    setDisplayMinMax();
+
+    setColorTemperature(temperatureIn, temperatureOut);
+
+    interiorDisplay.innerText = temperatureIn + '°C';
+    exteriorDisplay.innerText = temperatureOut + '°C';
+
+    addValueInGraph(temperatureIn, new Date(), temperatureOut, new Date());
+
+    // Affichage des problèmes
+    let allMsg = []
+
+    if (temperatureOut < 0) {
+        allMsg.push('Banquise en vue !');
+    } else if (35 < temperatureOut) {
+        allMsg.push('Hot ! Hot ! Hot !');
+    }
+
+    if (temperatureIn < 0) {
+        allMsg.push('Canalisations gelées, appelez SOS plombier et mettez un bonnet !');
+    } else if (0 <= temperatureIn && temperatureIn < 12) {
+        allMsg.push('Montez le chauffage ou mettez un gros pull  !');
+    } else if (22 < temperatureIn && temperatureIn <= 50) {
+        allMsg.push('Baissez le chauffage !');
+    } else if (50 <= temperatureIn) {
+        allMsg.push('Appelez les pompiers ou arrêtez votre barbecue !');
+    }
+
+    let alertDiv = document.getElementById('contentAlert');
+    // Remove toutes les alertes précédentes
+    while (alertDiv.lastElementChild) {
+        alertDiv.removeChild(alertDiv.lastElementChild);
+    }
+
+    // Ajout des nouvelles alertes
+    for (i in allMsg.length) {
+        alertDiv.appendChild(document.importNode(content, true));
+        let span = document.querySelectorAll('#contentAlert section:last-of-type span')[0];
+        span.innerText = allMsg[i];
+        // Changer l'affichage de la température
+        alertNotification(allMsg[i], temperatureIn);
+    }
+}
+
+//
+function setColorTemperature(temperatureIn, temperatureOut) {
+    // Couleur utilisé suivant la température extérieure
+    let color = 'white';
+    if (temperatureOut < 0)
+        color = 'blue';
+    else if (35 < temperatureOut)
+        color = 'red';
+    interiorDisplay.setAttribute("class", color);
+
+    // Couleur utilisé suivant la température intérieure
+    color = 'white'
+    if (temperatureIn < 0)
+        color = 'blue';
+    else if (0 <= temperatureIn && temperatureIn < 12)
+        color = 'cyan';
+    else if (22 <= temperatureIn && temperatureIn < 50)
+        color = 'orange';
+    else if (50 <= temperatureIn)
+        color = 'red';
+    else
+        color = 'white';
+    exteriorDisplay.setAttribute("class", color);
+}
 
 
 // --------------------------------------------------------------------------------------------- //
@@ -487,9 +521,12 @@ function initGraph() {
             responsive: true,
             scales: {
                 y: {
-                    min: 0,
+                    min: 0
                 },
                 x: {
+                    // callback: function (val, index) {
+                    //     return index % 2 === 0 ? this.getLabelForValue(val) : '';
+                    // },
                     type: 'time',
                     time: {
                         unit: 'hour',
@@ -501,38 +538,52 @@ function initGraph() {
                     scaleLabel: {
                         display: true,
                         labelString: 'Time'
-                    }
+                    },
                 }
             }
         }
     });
 
-    // Ajout des anciennes valeures mise en cache
-    let arraysCacheJSON = JSON.parse(localStorage.getItem('TEMPERATURE'));
-    let tempIn = arraysCacheJSON['IN_TEMP'];
-    let tempOut = arraysCacheJSON['OUT_TEMP'];
+    updateGraph();
 
-    for (var i = 0; i < Object.keys(tempIn['TEMP']).length; ++i) {
-        graph.data.datasets[0].data.push({ 'x': tempIn['TIME'][i], 'y': tempIn['TEMP'][i] });
-        graph.data.datasets[1].data.push({ 'x': tempIn['TIME'][i], 'y': tempOut['TEMP'][i] });
-    }
+    console.log("Initialisation graphe :", graph.data.datasets);
+}
+
+
+//
+function addValueInGraph(temperatureIn, dateIn, temperatureOut, dateOut) {
+    graph.data.datasets[0].data.push({ 'x': dateIn, 'y': temperatureIn });
+    graph.data.datasets[1].data.push({ 'x': dateOut, 'y': temperatureOut });
+
     graph.update();
-
-    console.log(graph.data.datasets);
 }
 
 
 //
 function updateGraph() {
-    let arraysCacheJSON = JSON.parse(localStorage.getItem('TEMPERATURE'));
-    let tempIn = arraysCacheJSON['IN_TEMP'];
-    let tempOut = arraysCacheJSON['OUT_TEMP'];
+    let temperatureIn = temperatureLocalStorageJSON['IN_TEMP'];
+    let temperatureOut = temperatureLocalStorageJSON['OUT_TEMP'];
+    let graphLenght = Object.keys(temperatureIn['TEMP']).length;
 
-    let i = Object.keys(tempIn['TEMP']).length - 1;
+    clearGraph();
 
-    graph.data.datasets[0].data.push({ 'x': tempIn['TIME'][i], 'y': tempIn['TEMP'][i] });
-    graph.data.datasets[1].data.push({ 'x': tempIn['TIME'][i], 'y': tempOut['TEMP'][i] });
+    // Ajout des valeurs du LocalStorage dans notre graphe
+    for (var i = 0; i < graphLenght; ++i) {
+        graph.data.datasets[0].data.push({ 'x': temperatureIn['TIME'][i], 'y': temperatureIn['TEMP'][i] });
+        graph.data.datasets[1].data.push({ 'x': temperatureOut['TIME'][i], 'y': temperatureOut['TEMP'][i] });
+    }
 
     graph.update();
 }
 
+
+//
+function clearGraph() {
+    let graphLenght = Object.keys(temperatureLocalStorageJSON['IN_TEMP']['TEMP']).length;
+
+    // On enlève toutes les valeurs du graphe 
+    graph.data.datasets[0].data.splice(0, graphLenght);
+    graph.data.datasets[1].data.splice(0, graphLenght);
+
+    graph.update();
+}
